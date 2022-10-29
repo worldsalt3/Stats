baeece438f762767b4e00a0bd1c3ecc4573f850f
Worldsalt3 <riliwanhazzan@gmail.com>
2022-10-29 22:29:03 +0100

    advance refactor on codebase

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,21 +1,33 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
+import { dateStringToDate } from './utils'
+import { MatchResult } from './MatchResult'
+import { MatchData } from './MatchData'
+import { CsvFileReader } from './CsvFileReader'
 
+interface DataReader {
+  read(): void
+  data: string[][]
+}
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
+export class MatchReader {
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename))
+  }
 
+  matches: MatchData[] = []
+  constructor(public reader: DataReader) {}
 
-export class matchReader extends CsvFileReader<MatchData> {
-    mapRow(row: string[]): MatchData {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ]
-    }
+  load(): void {
+    this.reader.read()
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ]
+    })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { matchReader } from './MatchReader'
-import { MatchResult } from './MatchResult'
-
-const reader = new matchReader('football.csv')
-reader.read()
+import { MatchReader } from './MatchReader'
+import { Summary } from './Summary'
 
 
-let manUnitedWins = 0
+const matchReader = MatchReader.fromCsv('football.csv')
+matchReader.load()
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`)
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
+summary.buildAndPrintReport(matchReader.matches)
